25a2d3572219e6e16c3d2d9d00386ead5ad60530
Mohammed Molla <mjmolla@gmail.com>
2023-10-28 16:02:22 -0400

    how to build maven, area

---
+++ src/main/java/classContain/WhatClassContain.java
@@ -0,0 +1,28 @@
+package classContain;
+
+public class WhatClassContain {
+
+	String fistName;
+	static String lastName;
+
+	public WhatClassContain(String fistName) {
+		super();
+		this.fistName = fistName;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+	public void fullName() {
+		int money = 5000;
+	}
+
+	{
+		System.out.print("hello");
+	}
+
+	static {
+		System.out.print(lastName = "Molla");
+	}
+}
---
+++ src/main/java/dataTypeInJava/FirstClassInJava.java
@@ -0,0 +1,20 @@
+package dataTypeInJava;
+
+public class FirstClassInJava {
+
+	public static void main(String[] args) {
+		// this is single line comments
+		
+		
+		/*
+		 * this is block of comments
+		 * multiple lines of comments
+		
+		 */
+		
+		System.out.println("Hello Java");
+		System.out.print("I am a java developer");
+
+	}
+ 
+}
